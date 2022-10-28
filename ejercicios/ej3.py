0e90ae2485cb3d8776e1b96816cc85b40316574e
Xavitheforce <javier.miguelez.yague@gmail.com>
2022-10-28 15:41:14 +0200

    Update ej3.py

--- ejercicios/ej3.py
+++ ejercicios/ej3.py
@@ -14,3 +14,10 @@ def ordenar_naves():
         naves.append(row['Name'])
     naves.sort()
     print(f"\n\nLas naves ordenadas por nombre son {naves}")
+
+def ordenar_largo():
+    naves_largo = []
+    for row in star_naves:
+        naves_largo.append(row['Name'] + " " + row['Length'])
+    naves_largo.sort() ; naves_largo.reverse()
+    print(f"\n\nLas naves ordenadas por largo son {naves_largo}")
