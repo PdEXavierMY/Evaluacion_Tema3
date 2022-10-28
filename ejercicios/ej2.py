c126ac8d96c935f30f7c0e56d9086a28aa6e07b0
Xavitheforce <javier.miguelez.yague@gmail.com>
2022-10-28 16:01:05 +0200

    cambois

--- ejercicios/ej2.py
+++ ejercicios/ej2.py
@@ -6,6 +6,21 @@ def construir_matriz_iterativo():
             matriz[i].append(numero)
     return matriz
 
+def construir_matriz_recursivo(n=0, fila=0, matriz=[[], [], []]):
+    if fila < 3:
+        if n < 3:
+            numero = int(input("Introduce un elemento de la matriz(por filas): "))
+            matriz[fila].append(numero)
+            construir_matriz_recursivo(n+1, fila, matriz)
+        else:
+            numero = int(input("Introduce un elemento de la matriz(por filas): "))
+            matriz[fila].append(numero)
+            construir_matriz_recursivo(0, fila+1, matriz)
+    else:
+        return matriz
 
 def regla_de_sarrus(matriz):
     return int((matriz[2][2]*matriz[1][1]*matriz[0][0])+(matriz[1][2]*matriz[0][1]*matriz[2][0])+(matriz[2][1]*matriz[1][0]*matriz[0][2])-(matriz[2][0]*matriz[1][1]*matriz[0][2])-(matriz[2][1]*matriz[1][2]*matriz[0][0])-(matriz[1][0]*matriz[0][1]*matriz[2][2]))
+
+ca = construir_matriz_recursivo()
+print(ca)
--- ejercicios/ej3.py
+++ ejercicios/ej3.py
@@ -15,6 +15,7 @@ def ordenar_naves():
     naves.sort()
     print(f"\n\nLas naves ordenadas por nombre son {naves}")
 
+
 def ordenar_largo():
     naves_largo = []
     for row in star_naves:
@@ -28,23 +29,3 @@ def millenium_falcon():
         if row['Name'] == "Millennium Falcon":
             print(f"\n\nLa información del Millenium Falcon es {row}")
 
-def death_star():
-    for row in star_naves:
-        if row['Name'] == "Death Star":
-            print(f"\n\nLa información de la Death Star es {row}")
-
-
-def cinco_naves():
-    naves = []
-    for row in star_naves:
-        naves.append(row['Name'] + " " + row['Passengers'])
-    naves.sort() ; naves.reverse()
-    print(f"\n\nLas cinco naves con mayor cantidad de pasajeros son {naves[:5]}")
-
-
-def mayor_tripulacion():
-    naves = []
-    for row in star_naves:
-        naves.append(row['Name'] + " " + row['Crew'])
-    naves.sort() ; naves.reverse()
-    print(f"\n\nLa nave que requiere mayor cantidad de tripulación es {naves[0]}")
